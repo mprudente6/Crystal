7406c6fcb78450380252a6fc339cb5973a7b1b45
Maurizio <m.prudente@grifomultimedia.it>
2026-04-17 16:56:14 +0200

    Carousel and gallery

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
+import { Carousel } from './carousel/carousel';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Navbar],
+  imports: [RouterOutlet,Navbar,Carousel],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/carousel/carousel.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-carousel',
+  imports: [],
+  templateUrl: './carousel.html',
+  styleUrl: './carousel.css',
+})
+export class Carousel {}
